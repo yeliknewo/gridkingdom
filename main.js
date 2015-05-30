003377b29244d2235445319e4126809b3b530535
kileyowen <yelikdev@gmail.com>
2015-05-30 14:06:45 -0400

    Eye Test

--- main.js
+++ main.js
@@ -1,31 +1,30 @@
 var Pixi = require('pixi.js');
 
-var renderer, updateDelta, lastUpdate, gameContainer, background;
+var renderer, updateDelta, lastUpdate, gameContainer, loader, eyebackground, pupil, pupilOrigin, input;
 
 window.onload = new function(){
 	renderer = Pixi.autoDetectRenderer(800, 600);
+	input = new Pixi.interaction.InteractionManager(renderer);
 	gameContainer = new Pixi.Container();
 	document.body.appendChild(renderer.view);
+	pupilOrigin = new Pixi.Point(renderer.width / 2, renderer.height / 2);
 	
-	background = new Pixi.Graphics();
-	
-	var bgRect = new Pixi.Rectangle(0, 0, renderer.width, renderer.height);
-	background.beginFill(0xff00ff, 0xff);
-	background.drawShape(bgRect);
-	background.endFill();
-	
-	var bgCircle = new Pixi.Circle(renderer.width / 2, renderer.height / 2, Math.min(renderer.width / 2, renderer.height / 2));
-	background.beginFill(0x00ff00, 0xff);
-	background.drawShape(bgCircle);
-	background.endFill();
-	
-	gameContainer.addChild(background);
+	loader = new Pixi.loaders.Loader('./../assets/');
+	loader.add('eyebackground', 'eyebackground.png').add('pupil', 'pupil.png').load(function(loader, resources){
+		
+		eyebackground = new Pixi.Sprite(resources.eyebackground.texture);
+		gameContainer.addChild(eyebackground);
+		
+		pupil = new Pixi.Sprite(resources.pupil.texture);
+		pupil.anchor = new Pixi.Point(.5, .5);
+		gameContainer.addChild(pupil);
+			
+		requestAnimationFrame(run);
+	});
 	
 	var updatesPerSecond = 40;
 	updateDelta = 1000 / updatesPerSecond;
 	lastUpdate = 0;
-	
-	requestAnimationFrame(run);
 }
 
 function run(timestamp){
@@ -38,7 +37,7 @@ function run(timestamp){
 }
 
 function update(){
-	
+	pupil.position.set(pupilOrigin.x + (input.mouse.global.x - pupilOrigin.x) / 2, pupilOrigin.y + (input.mouse.global.y - pupilOrigin.y) / 2);
 }
 
 function render(){
